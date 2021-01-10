850500bcf0f96b0c48526b8b1bcb1a08635fdf4f
yogi <raoyogesh78@yahoo.in>
2021-01-10 10:38:04 +0100

    update component using props

--- src/Comment.js
+++ src/Comment.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-function Comment() {
+function Comment({author, avatar, time, text}) {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={Faker.image.people()} />
+                <img alt="avatar" src={avatar} />
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Yogi
+                    {author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{time}</span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{text}</div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
+import Comment from './Comment';
 
 const App = () => {
     return (
         <div className="ui container comments">
-           
+           <Comment author={Faker.name.firstName()} avatar={Faker.image.people()} time="Today at 6:00PM" text="Nice post!" />
+           <Comment author={Faker.name.firstName()} avatar={Faker.image.people()} time="Yesterday at 9:00PM" text="I like your post." />
+           <Comment author={Faker.name.firstName()} avatar={Faker.image.people()} time="Today at 7:57AM" text="Great work!" />
         </div>
     );
 };
